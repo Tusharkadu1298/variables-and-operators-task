b51fe6f1f8098762d9e16b78cd5493175e4dd1c9
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-05-05 15:46:21 +0530

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+# 4. Apply all the types of Arithmetic Operators on the following Variables:
+#   a = 40
+#   b = 5
+
+
+a = 40
+b = 5
+print(a + b)                # addition 
+print(a - b)                # minus
+print(a * b)                # multipy
+print(a / b)                # divide
+print(a // b)               # floor division
+print(a % b)                # modulus
+print(a ** 2)               # exponation
+print(b ** 2)               # exponation
