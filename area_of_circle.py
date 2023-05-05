e5d280917366a46c8d6f3292a164a1b48e861107
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-05-05 15:37:00 +0530

    Create area_of_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,6 @@
+#1. write a program to find the area of a circle, take the input from the user for the radius
+
+print("find the area of circle")
+radius = int(input("enter the radius :-- "))
+radius = (22/7)*(radius*radius)
+print("your radius is :--",radius) 
